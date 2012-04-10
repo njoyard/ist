09afea197ce3032fd1da72be951768264a82c55a
Nicolas Joyard <joyard.nicolas@gmail.com>
2012-04-10 22:28:21 +0200

    Initial release

---
+++ README.markdown
@@ -0,0 +1,205 @@
+# [IST](http://github.com/k-o-x/ist) - Indented Selector Templating
+
+## Introduction
+
+IST is a DOM templating engine using a CSS selector-like syntax.  Templates are
+text files, which are first parsed and compiled into a template object, and then
+rendered into a DOM document using a context object.  This file documents usage
+of version 0.1.
+
+Here is a brief overview of an IST template file:
+
+	div#content
+	    @each articles
+	        div.article
+	            h1
+	                "{{ title }}"
+	            "{{ text }}"
+	        @unless comments.length
+	            "No comments for now !"
+	        @each comments
+	            div.comment
+	                "{{ this }}"
+	        form.newcomment
+	            label[for=name]
+	                "Name :"
+	            input.commentInput#name[type=text]
+	            label[for=comment]
+	            textarea[cols=55]#comment
+	                "Enter your comment here"
+
+## Usage
+
+### Template compilation.
+
+IST can be used either as an AMD module or as a RequireJS plugin.  To compile a
+template string, use the module syntax as follows:
+
+	require(['ist'], function(ist) {
+		var myTemplate = ist('div#myId.myClass[myProp=myVal]');
+		...
+	});
+	
+To compile a template file, you can use the RequireJS plugin syntax:
+
+	require(['ist!path/to/template'], function(myTemplate) {
+		...
+	});
+
+Note that the plugin automatically adds an `.ist` extension to file names.
+
+### Rendering
+
+You can render a template using the `render` method of compiled templates.  By
+defaut, it will render using the global object `document` property (eg.
+`window.document`).
+
+	document.body.appendChild(myTemplate.render({ articles: [...] }));
+	
+When rendering to an other document, you can pass it as a second argument:
+
+	popup.document.body.appendChild(myTemplate.render({ ... }, popup.document));
+
+## Template syntax
+
+### Basics
+
+All nodes are specified on a separate line.  Empty lines (other than whitespace)
+are ignored by IST.  The tree structure is specified using indentation.  You can
+indent using (any number of) either spaces or tabs, but you have to be
+consistent.  An increase in indent means a parent-child relationship:
+
+	div.parent
+	    div.child
+		
+Likewise, node with the same indent are siblings:
+
+	div.parent
+	    div.child
+	    div.child
+		
+When decreasing indent, be careful to match a previous line with the same indent
+level:
+
+	div.parent
+	    div.child
+	  div.invalid
+
+	div.parent
+	    div.child
+	div.valid
+	  
+There is no restriction in the size of indent. The following are all equivalent:
+
+	div.parent
+	 div.parent
+
+	div.parent
+	             div.child
+
+	div.parent
+		div.child
+
+However, indent is compared strictly: it has to be strictly identical for all
+nodes at the same level.  Therefore it's best to avoid mixing tabs and spaces.
+
+### Element nodes - Selectors
+
+CSS-like selectors are used to specify element nodes.  The element tag name is
+always specified first, and qualifiers follow, in any order, but without
+spacing. You can use ID qualifiers :
+
+	div#an_id
+
+class qualifiers:
+
+	span.a_class
+	
+and attribute qualifiers:
+
+	input[type=text]
+	
+All those can be mixed in any order:
+
+	div#id.class1[style=display: none;].class2#final_id
+	
+Note that when specifying multiple IDs, only the last one is kept.  Finally, for
+some corner-cases, properties can be specified as follows:
+
+	div[.className=one two three]
+
+### Text nodes
+
+Text nodes are specified with double quotes:
+
+	"top-level text node"
+	div.text
+		"child text node"
+		"any character is valid here including "double quotes""
+		"the final double quote is optional
+
+Of course text nodes cannot have any child nodes.
+
+### String interpolation
+
+You can insert values from the rendering context using double curly braces in
+text nodes or in property/attribute values:
+
+	"value is {{ value }}, bar is {{ bar }}"
+	div[.className={{ cssClass1 }} {{ cssClass2 }}]
+		"{{ access.to.sub.properties }}"
+		"Spaces inside braces are {{ignored     }}"
+		"The special keyword {{ this }} references the current context itself"
+
+Using string interpolation is not possible with ID or class qualifiers, but you
+can use `[.className={{ variable }}]` and `[.id={{ variable }}]`
+
+### Directives
+
+Directives are used to control node generation with the help of context
+properties.  The syntax of both directives is the same:
+
+	@directive path.to.context.property
+	
+Conditional directives enable conditional rendering of a subtree:
+
+	@if some.property.is.truthy
+		div.willBeRendered
+		
+	@unless some.property.is.truthy
+		div.willNotBeRendered
+
+The `@each` loop directive iterates over an array.  Context for the subtree is
+switched to each of the array elements in turn:
+
+	@each articles
+		h1
+			"{{ title }}"
+		div
+			"{{ content }}"
+
+Finally, the `@with` directive enables context narrowing:
+
+	span
+		"{{ deeply.nested.object.property1 }}"
+		"{{ deeply.nested.object.property2 }}"
+		
+	div.clearer
+		@with deeply.nested.object
+			"{{ property1 }}"
+			"{{ property2 }}"
+
+## Planned (thus missing) features
+
+The following features will be included in future versions:
+
+- comments
+- external template inclusion
+- better text node handling (esp. for escaped chars such as \n & \t)
+
+## License
+
+IST is distributed under the MIT license. See the file
+[`LICENSE`](http://github.com/k-o-x/ist) for more information.
+
+Copyright (c) 2012 Nicolas Joyard
---
+++ ist.js
@@ -0,0 +1,487 @@
+/** @license
+ * IST: Indented Selector Templating
+ * version 0.1 - require plugin
+ *
+ * Copyright (c) 2012 Nicolas Joyard
+ * Released under the MIT license.
+ *
+ * Author: Nicolas Joyard <joyard.nicolas@gmail.com>
+ * http://github.com/k-o-x/ist
+ *
+ * Code shamelessly inspired from Alex Sexton's hbs plugin
+ * Only works in a browser environment for now
+ */
+
+/*jslint white: true, browser: true, plusplus: true */
+/*global define, require, ActiveXObject */
+
+define('ist', [], function () {
+	"use strict";
+
+	var fs, 
+		progIds = ['Msxml2.XMLHTTP', 'Microsoft.XMLHTTP', 'Msxml2.XMLHTTP.4.0'],
+		buildMap = [];
+		
+		
+	/******************************************
+	 *            Actual IST code             *
+	 ******************************************/
+		
+	/**
+	 * Find path ("path.to.property") inside context object
+	 */
+	function findPath(path, context) {
+		path = path.trim();
+		if (path === 'this') {
+			return context;
+		}
+	
+		function rec(pathArray, context) {
+			var subcontext = context[pathArray.shift()];
+			
+			if (pathArray.length > 0) {
+				return rec(pathArray, subcontext);
+			} else {
+				return subcontext;
+			}
+		}
+		
+		try {
+			return rec(path.split('.'), context);
+		} catch(e) {
+			throw new Error("Cannot find path " + path + " in context");
+		}
+	};
+	
+	/**
+	 * Interpolate {{variable}}s in 'text'
+	 */
+	function interpolate(text, context) {
+		return text.replace(/{{(.*?)}}/g, function(m, p1) { return findPath(p1, context); });
+	};
+	
+	
+	/**
+	 * Text node
+	 */
+	function TextNode(text) {
+		this.text = text;
+	};
+	
+	TextNode.prototype = {
+		render: function(context, doc) {
+			return (doc||document).createTextNode(interpolate(this.text, context));
+		},
+		
+		appendChild: function(node) {
+			throw new Error("Cannot add children to TextNode");
+		}
+	};
+	
+	
+	/**
+	 * Container node
+	 */
+	function ContainerNode(features) {
+		var self = this;
+		
+		if (features) {
+			Object.keys(features).forEach(function(key) {
+				self[key] = features[key];
+			});
+		}
+	};
+	
+	ContainerNode.prototype = {
+		appendChild: function(node) {
+			this.children = this.children || [];
+			this.children.push(node);
+		},
+		
+		render: function(context, doc) {
+			var fragment = (doc||document).createDocumentFragment();
+			
+			this.children = this.children || [];
+			this.children.forEach(function(c) {
+				fragment.appendChild(c.render(context, doc));
+			});
+			
+			return fragment;
+		}
+	};
+	
+	
+	/**
+	 * Element node
+	 */
+	function ElementNode(tagName) {
+		this.tagName = tagName;
+		this.attributes = {};
+		this.classes = [];
+		this.id = undefined;
+	};
+	
+	ElementNode.prototype = new ContainerNode({
+		setAttribute: function(attr, value) {
+			this.attributes[attr] = value;
+		},
+		
+		setClass: function(cls) {
+			this.classes.push(cls);
+		},
+		
+		setId: function(id) {
+			this.id = id;
+		},
+		
+		render: function(context, doc) {
+			var self = this,
+				node = (doc||document).createElement(this.tagName);
+			
+			node.appendChild(ContainerNode.prototype.render.call(this, context, doc));
+			
+			Object.keys(this.attributes).forEach(function(attr) {
+				var value = interpolate(self.attributes[attr],  context);
+				
+				if (attr.charAt(0) === '.') {
+					// Property
+					node[attr.substr(1)] = value;
+				} else {
+					// Attribute
+					node.setAttribute(attr, value);
+				}
+				
+			});
+			
+			this.classes.forEach(function(cls) {
+				node.classList.add(cls);
+			});
+			
+			if (typeof this.id !== 'undefined') {
+				node.id = this.id;
+			}
+			
+			return node;
+		}
+	});
+	
+	
+	/**
+	 * Each node
+	 */
+	function EachNode(loopPath) {
+		this.loopPath = loopPath;
+	};
+	
+	EachNode.prototype = new ContainerNode({
+		render: function(context, doc) {
+			var self = this, 
+				loopOver = findPath(this.loopPath, context),
+				fragment = (doc||document).createDocumentFragment();
+			
+			loopOver.forEach(function(subcontext) {
+				fragment.appendChild(ContainerNode.prototype.render.call(self, subcontext, doc));
+			});
+			
+			return fragment;
+		}
+	});
+	
+	
+	/**
+	 * With node
+	 */
+	function WithNode(ctxPath) {
+		this.ctxPath = ctxPath
+	};
+	
+	WithNode.prototype = new ContainerNode({
+		render: function(context, doc) {
+			var subcontext = findPath(this.ctxPath, context);
+			return ContainerNode.prototype.render.call(this, subcontext, doc);
+		}
+	});
+	
+	
+	/**
+	 * If node
+	 */
+	function IfNode(condPath) {
+		this.condPath = condPath;
+	};
+
+	IfNode.prototype = new ContainerNode({
+		render: function(context, doc) {
+			var condition = findPath(this.ctxPath, context);
+			
+			if (condition) {
+				return ContainerNode.prototype.render.call(this, context, doc);
+			} else {
+				return (doc||document).createDocumentFragment();
+			}
+		}
+	});
+	
+	
+	/**
+	 * Unless node
+	 */
+	function UnlessNode(condPath) {
+		this.condPath = condPath;
+	};
+
+	UnlessNode.prototype = new ContainerNode({
+		render: function(context, doc) {
+			var condition = findPath(this.ctxPath, context);
+			
+			if (!condition) {
+				return ContainerNode.prototype.render.call(this, context, doc);
+			} else {
+				return (doc||document).createDocumentFragment();
+			}
+		}
+	});
+	
+	
+	/**
+	 * Template compiler
+	 * Returns ContainerNode with render(context[, document]) method
+	 */
+	function ist(text, name) {
+		var lines = text.split('\n'),
+			rx = {
+				indent: /^(\s*)(.*)$/,
+				element: /^([a-z0-9]+)(.*)$/,
+					elemProps: /^([.#][a-zA-Z0-9_-]+|\[[^\]=]+=[^\]]+\])(.*)$/,
+					elemClass: /^\.([a-zA-Z0-9_-]+)$/,
+					elemId: /^#([a-zA-Z0-9_-]+)$/,
+					elemAttr: /^\[([^\]=]+)=([^\]]+)\]$/,
+				text: /^"([^"]*?)"?$/,
+				directive: /^@(if|unless|with|each)\s+(.*)$/
+			},
+			dirCtors = {
+				'if': IfNode,
+				'unless': UnlessNode,
+				'with': WithNode,
+				'each': EachNode
+			},
+			stack = [new ContainerNode()],
+			indentStack;
+		
+		
+		name = name || '<unknown>';
+	
+		function peekNode() {
+			return stack[stack.length - 1];
+		};
+	
+		// Push a node on the stack
+		function pushNode(node) {
+			stack.push(node);
+		};
+	
+		// Pop a node from the stack and add it to previous element children
+		function popNode() {
+			var node;
+			if (stack.length < 2) {
+				throw new Error("Could not pop node from stack");
+			}
+		
+			node = stack.pop();
+			peekNode().appendChild(node);
+			return node;
+		};
+	
+		lines.forEach(function(line, lineNumber) {
+			var m, node, prop, rest, lastIndent, indentIdx;
+		
+			m = line.match(rx.indent);
+		
+			// Skip empty lines
+			if (m[2].length === 0) {
+				return;
+			} 
+		
+			try {
+				// Handle indent
+				if (typeof indentStack === 'undefined') {
+					indentStack = [m[1]];
+				} else {
+					lastIndent = indentStack[indentStack.length - 1];
+					indentIdx = indentStack.indexOf(m[1]);
+			
+					if (m[1] !== lastIndent) {
+						if (m[1].indexOf(lastIndent) === 0) {
+							// Indent growed, push new indent
+							indentStack.push(m[1]);
+						} else if (indentIdx !== -1) {
+							// Return to previous indent, popping nodes in their parent
+							popNode();
+						
+							while (indentStack.length - 1> indentIdx) {
+								indentStack.pop();
+								popNode();
+							}
+						} else {
+							throw new Error("Unexpected indent");
+						}
+					} else {
+						// Indent unchanged, pop node from stack into parent (it won't have any children)
+						popNode();
+					}
+				}
+				
+				rest = m[2];
+		
+				// Handle actual node
+				if (m = rest.match(rx.element)) {
+					node = new ElementNode(m[1]);
+					rest = m[2];
+			
+					while (rest.length) {
+						if (m = rest.match(rx.elemProps)) {
+							prop = m[1];
+							rest = m[2];
+					
+							if (m = prop.match(rx.elemClass)) {
+								node.setClass(m[1]);
+							} else if (m = prop.match(rx.elemId)) {
+								node.setId(m[1]);
+							} else if (m = prop.match(rx.elemAttr)) {
+								node.setAttribute(m[1], m[2]);
+							}
+						} else {
+							throw new Error("Invalid syntax (character " + (line.length - rest.length) + ")");
+						}
+					}
+			
+					pushNode(node);
+				} else if (m = rest.match(rx.text)) {
+					pushNode(new TextNode(m[1]));
+				} else if (m = rest.match(rx.directive)) {
+					pushNode(new dirCtors[m[1]](m[2]));
+				} else {
+					throw new Error("Invalid syntax (character 0)");
+				}
+			} catch(e) {
+				throw new Error(e.message + ' in ' + name + ' on line ' + (lineNumber+1));
+			}
+		});
+		
+		// Collapse remaining stack
+		while (stack.length > 1) {
+			popNode();
+		}
+		
+		return peekNode();
+	};
+
+
+	/******************************************
+	 *         Require plugin helpers         *
+	 ******************************************/
+
+	function getXhr() {
+		var xhr, i, progId;
+		if (typeof XMLHttpRequest !== "undefined") {
+			return new XMLHttpRequest();
+		} else {
+			for (i = 0; i < 3; i++) {
+				progId = progIds[i];
+				try {
+					xhr = new ActiveXObject(progId);
+				} catch (e) {}
+
+				if (xhr) {
+					progIds = [progId];  // faster next time
+					break;
+				}
+			}
+		}
+
+		if (!xhr) {
+			throw new Error("getXhr(): XMLHttpRequest not available");
+		}
+
+		return xhr;
+	};
+
+	function fetchText(url, callback) {
+		var xhr = getXhr();
+		xhr.open('GET', url, true);
+		xhr.onreadystatechange = function (evt) {
+			//Do not explicitly handle errors, those should be
+			//visible via console output in the browser.
+			if (xhr.readyState === 4) {
+				callback(xhr.responseText);
+			}
+		};
+		xhr.send(null);
+	};
+
+
+	function jsEscape (content) {
+		return content.replace(/(['\\])/g, '\\$1')
+			.replace(/[\f]/g, "\\f")
+			.replace(/[\b]/g, "\\b")
+			.replace(/[\t]/g, "\\t")
+			.replace(/[\n]/g, "\\n' +\n\t\t'")
+			.replace(/[\r]/g, "\\r");
+	};
+	
+
+	/******************************************
+	 *        Require plugin interface        *
+	 ******************************************/
+	
+	ist.get = function () {
+		return domton;
+	};
+
+	ist.write = function (pluginName, name, write) {
+		if (buildMap.hasOwnProperty(name)) {
+			var text = buildMap[name];
+			write(text);
+		}
+	};
+
+	ist.version = '0.1';
+
+	ist.load = function (name, parentRequire, load, config) {
+		var path = parentRequire.toUrl(name + '.ist');
+		
+		fetchText(path, function (text) {
+			var i;
+			
+			text = "/* START_TEMPLATE */\n" + 
+				   "define('ist!" + name + "',['ist'], function(ist){ \n" +
+				   "\tvar template = '" + jsEscape(text) + "';\n" +
+				   "\treturn ist(template, '" + name + "');\n" +
+				   "});\n";
+				   
+			//Hold on to the transformed text if a build.
+			if (config.isBuild) {
+				buildMap[name] = text;
+			}
+
+			//IE with conditional comments on cannot handle the
+			//sourceURL trick, so skip it if enabled.
+			/*@if (@_jscript) @else @*/
+			if (!config.isBuild) {
+				text += "\r\n//@ sourceURL=" + path;
+			}
+			/*@end@*/
+			
+			load.fromText(name, text);
+
+			//Give result to load. Need to wait until the module
+			//is fully parse, which will happen after this
+			//execution.
+			parentRequire([name], function (value) {
+				load(value);
+			});
+		});
+	};
+	
+	return ist;
+});
