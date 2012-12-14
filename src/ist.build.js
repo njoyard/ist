d672bd0ddfa0bba0ea8d0be4456840027b2b3e01
Nicolas Joyard <joyard.nicolas@gmail.com>
2012-12-14 08:04:39 +0100

    Add indentation wrapper to build stage

--- src/ist.build.js
+++ src/ist.build.js
@@ -1,7 +1,12 @@
 {
-	baseUrl: '.',
 	name: 'ist',
+	
 	optimize: 'uglify2',
+	
+	onBuildWrite: function (moduleName, path, contents) {
+        return contents.replace(/^/mg, '\t');
+    },
+    
 	wrap: {
 		startFile: 'parts/build.header.part',
 		endFile: 'parts/build.footer.part'
