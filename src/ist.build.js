a3afbcf108742a49df011dab69564a729e9b4b15
Nicolas Joyard <joyard.nicolas@gmail.com>
2012-12-15 15:27:14 +0100

    Update build to replace define calls

--- src/ist.build.js
+++ src/ist.build.js
@@ -4,7 +4,19 @@
 	optimize: 'uglify2',
 	
 	onBuildWrite: function (moduleName, path, contents) {
-        return contents.replace(/^/mg, '\t');
+		// Replace define calls
+		return contents.replace(
+			/define\s*\(["']([^"']+)["']\s*,\s*\[((?:\n|[^\]])*)\]\s*,((?:\n|.)*)\)/g,
+			function(match, p1, p2, p3) {
+				var deps = p2.replace(
+						/["']([^"']+)["']/g, 
+						function(match, p1) {
+							return "modules[\"" + p1 + "\"]";
+						}
+					);
+				return "modules[\"" + p1 + "\"] = (" + p3 + "(" + deps + "))";
+			}
+		).replace(/^/mg, '\t');
     },
     
 	wrap: {
