5919fc95bc48874521d6c45184ef5f2a6a3b5a53
Nicolas Joyard <joyard.nicolas@gmail.com>
2012-12-16 19:27:24 +0100

    Cleanup module names

--- src/ist.build.js
+++ src/ist.build.js
@@ -4,18 +4,30 @@
 	optimize: 'uglify2',
 	
 	onBuildWrite: function (moduleName, path, contents) {
+		var mangleModuleName,
+			replaceModuleCalls,
+			replaceDefineCalls;
+			
+		mangleModuleName = function(name) {
+			return name.split('/').pop();
+		};
+			
+		replaceModuleCalls = function(match, p1) {
+			return "istComponents." + mangleModuleName(p1);
+		};
+		
+		replaceDefineCalls = function(match, p1, p2, p3) {
+			var deps = p2.replace(
+					/["']([^"']+)["']/g, 
+					replaceModuleCalls
+				);
+			return "istComponents." + mangleModuleName(p1) + " = (" + p3 + "(" + deps + "))";
+		};
+	
 		// Replace define calls
 		return contents.replace(
 			/define\s*\(["']([^"']+)["']\s*,\s*\[((?:\n|[^\]])*)\]\s*,((?:\n|.)*)\)/g,
-			function(match, p1, p2, p3) {
-				var deps = p2.replace(
-						/["']([^"']+)["']/g, 
-						function(match, p1) {
-							return "modules[\"" + p1 + "\"]";
-						}
-					);
-				return "modules[\"" + p1 + "\"] = (" + p3 + "(" + deps + "))";
-			}
+			replaceDefineCalls
 		).replace(/^/mg, '\t');
     },
     
